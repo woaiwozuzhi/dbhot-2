e93fb1d3d1e7c37ed3ba576f6ad695c86a6b598c
bigbog007 <1956309845@qq.com>
2024-05-22 15:29:48 +0800

    leader

---
+++ dbhot.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Good morning.\n");
+	printf("xiangrikui.\n");
+	printf("intelligent robot.\n");
+	printf("bug fixed.\n");
+	printf("add a function.\n");
+	printf("maintenance.\n");
+<<<<<<< HEAD
+	printf("beyond\n");
+=======
+	printf("compare.\n");
+>>>>>>> compare
+	return 0;
+}
